92e52fac89fa6a4f486897a881aa393852327b73
amrutsabale <samrut355@gmail.com>
2020-08-23 12:32:27 +0530

    rendered jobs using Job component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import useFetchjobs from "./useFetchjobs";
+import Job from "./Job";
+import { Container } from "react-bootstrap";
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+
+  const { jobs, loading, error } = useFetchjobs(params, page);
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <Container>
+      {loading && <h2>...Loading</h2>}
+      {error && <h2>Error occured ..refresh agains</h2>}
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
+    </Container>
   );
 }
 
