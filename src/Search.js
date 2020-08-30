69fcb6d342320bfe186ebfca2f09b26b99581118
amrutsabale <samrut355@gmail.com>
2020-08-30 13:08:17 +0530

    implemented search by location and full time

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,7 @@ import { Form, Col } from "react-bootstrap";
 export default function Search({ params, onParamChange }) {
   return (
     <Form className="mb-4">
-      <Form.Row>
+      <Form.Row className="align-items-end">
         <Form.Group as={Col}>
           <Form.Label>Description</Form.Label>
           <Form.Control
@@ -12,6 +12,26 @@ export default function Search({ params, onParamChange }) {
             onChange={onParamChange}
             name="description"
             value={params.description}
+          />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={onParamChange}
+            name="location"
+            value={params.location}
+          />
+        </Form.Group>
+        <Form.Group as={Col} xs="auto">
+          <Form.Check
+            id="full-time"
+            className="mb-2"
+            label="Only Full Time Jobs"
+            type="checkbox"
+            onChange={onParamChange}
+            name="full_time"
+            value={params.full_time}
           />
         </Form.Group>
       </Form.Row>
